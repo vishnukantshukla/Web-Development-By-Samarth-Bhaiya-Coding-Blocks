e3082af001b214e0fde33986f6e91e9838420a2b
vishnukantshukla <vishnukantshukla820@gmail.com>
2024-08-10 23:17:18 +0530

    updated files

--- JavaScript/Lec14/classList/app.js
+++ JavaScript/Lec14/classList/app.js
@@ -1,3 +1,5 @@
+// Note-> in case of adding classes ham har bare sare likhe likhne padhte jaab bhi ham new classes add karte but in case of classList ham aak baar likhe gye and jaab bhi hame new class add karne hogi to sirf bahi class add karayege
+
 // 1-> add method
 
 let arti= document.querySelector('article');
@@ -7,7 +9,7 @@ let arti= document.querySelector('article');
 arti.classList.add('vishnu','vishnu1','vishnu2'); // we can give multiple class by comma separated
 
 
-arti.classList.add('vishnu3') // in terms ko classes ham kisi bhi class ko kisi point of time me add bhi kar skte hai
+arti.classList.add('vishnu3') // in terms ko classes ham kisi bhi class ko kisi point of time me add bhi kar skte hai and hame purane class lo likhna nhi padega
 
 
 // 2- remove
@@ -16,3 +18,20 @@ arti.classList.add('vishnu3') // in terms ko classes ham kisi bhi class ko kisi 
 // arti.classList.remove('vishnu3') // jaab hame single class reove karne ho
 
 arti.classList.remove('vishnu3','vishnu2') 
+
+
+// 3- toggle
+
+arti.classList.toggle('toggle');  //true
+ // Agar toggle class hogi already to uske prooperties ko remove kar dega and agar toggle class nhi hue to toggle class ko add kar dega(uski properties add kar dega (bracket ke andar 'toggle' yeh class hai))
+
+
+// arti.classList.toggle('toggle'); //false (already add ho chuka hai to ham uski properties remove kar dega)
+
+
+// 4- contains
+
+console.log(arti.classList.contains('vishnu'));// output -> true  // yeh true or false me output dega
+ 
+
+// console.log(arti.classList.contains('vishnu'));  // output false
