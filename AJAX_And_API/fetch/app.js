1ceccb41bde011a22eaf9bcfe7f4603ef3d14c05
vishnukantshukla <vishnukantshukla820@gmail.com>
2024-10-04 02:59:30 +0530

    updated files

---
+++ AJAX_And_API/fetch/app.js
@@ -0,0 +1,21 @@
+// Method 2 ->  How to get data using fetch()
+// using fetch for url call
+
+fetch('https://api.tvmaze.com/search/shows?q=girls')
+    .then(function(response){   // yeh response ke sath resolve hua hoga
+        console.log(response);// yesme hame abhi pura data nhi mila hai meta data/header mila hai
+
+        return response.json(); // it also returns promise and yeh hame complete data dega .then() me jaab resolve hoga
+        console.log(data);
+        
+        
+    })
+    .then(function(data){  // agar yeh .json resolve hoga to yeh kucch data se resolve hoga
+
+        console.log(data);
+        
+    })
+    .catch(function(error){
+        console.log(error); 
+        
+    })
